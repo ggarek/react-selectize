f4dd1cfc292d5adb95fa055e81c51c9e97bf7e18
igor.ovsyannikov <igor.ovsyannikov@altspace.com>
2014-04-07 16:24:34 +0400

    [fix] fixed dropdown reopenning onchange

--- src/react-selectize.js
+++ src/react-selectize.js
@@ -9,6 +9,11 @@ var ReactSelectize = React.createClass({
     create: false
   },
 
+  isMultiple: function (props) {
+    // Selectize becomes 'multiple' when 'maxItems' is passed via settings
+    return props.multiple || props.maxItems != undefined;
+  },
+
   buildOptions: function () {
     var o = {},
       d = this._defaults;
@@ -16,8 +21,7 @@ var ReactSelectize = React.createClass({
     o.valueField = this.props.valueField || d.valueField;
     o.labelField = this.props.labelField || d.labelField;
     o.searchField = this.props.searchField || d.searchField;
-    if(this.props.multiple || this.props.maxItems != undefined){
-      // Selectize becomes 'multiple' when 'maxItems' is passed via settings
+    if(this.isMultiple(this.props)){
       o.maxItems = this.props.maxItems || null;
     }
     o.options = this.props.items || [];
@@ -26,10 +30,30 @@ var ReactSelectize = React.createClass({
     return o;
   },
 
-  rebuildSelectize: function () {
+  getSelectizeControl: function () {
     var selectId = "#" + this.props.selectId,
       $select = $(selectId),
-      selectControl = $select[0] && $select[0].selectize,
+      selectControl = $select[0] && $select[0].selectize;
+
+    return selectControl;
+  },
+
+  handleChange: function (e) {
+
+    // IF Selectize is not multiple
+    if(!this.isMultiple(this.props)){
+      // THEN blur it before calling onChange to prevent dropdown reopening
+      this.getSelectizeControl().blur();
+    }
+
+    if(this.props.onChange){
+      this.props.onChange(e);
+    }
+  },
+
+  rebuildSelectize: function () {
+    var $select = null,
+      selectControl = this.getSelectizeControl(),
       items = this.props.items;
 
     if(selectControl) {
@@ -39,13 +63,14 @@ var ReactSelectize = React.createClass({
       selectControl.load(function (cb) { cb(items) });
     } else {
       // build new
-      $select = $select.selectize(this.buildOptions());
+      $select = $("#" + this.props.selectId).selectize(this.buildOptions());
       selectControl = $select[0].selectize;
     }
 
     selectControl.setValue(this.props.value);
+
     if(this.props.onChange){
-      selectControl.on('change', this.props.onChange);
+      selectControl.on('change', this.handleChange);
     }
   },
 
