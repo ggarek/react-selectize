0c5aebb3207ae4d1daee03f8cbcf04353643c2e2
Michael Romanenko <michael@romanenko.kg>
2014-12-17 22:36:25 +0600

    Rename for label attribute to htmlFor

--- dist/react-selectize.js
+++ dist/react-selectize.js
@@ -90,7 +90,7 @@ var ReactSelectize = React.createClass({displayName: 'ReactSelectize',
   render: function () {
     var classes = this.props.classes;
     return React.DOM.div( {className:classes && classes.length > 0 ? classes.join(' ') : ''},
-      React.DOM.label( {for:this.props.selectId}, this.props.label),
+      React.DOM.label( {htmlFor:this.props.selectId}, this.props.label),
       React.DOM.select( {id:this.props.selectId, placeholder:this.props.placeholder})
     )
   }
