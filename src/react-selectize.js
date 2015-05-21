1f8c8d4d0234aec07e37d126c075f4f632455805
aleemb <aleemb@gmail.com>
2015-05-21 13:05:07 +0500

    Remove JSX dependency
    Removing JSX dependency makes it easier to use it with Node + require (webpack, etc) without running through a preprocessor. Quite a few other react component take this same route.

--- src/react-selectize.js
+++ src/react-selectize.js
@@ -89,9 +89,9 @@ var ReactSelectize = React.createClass({
 
   render: function () {
     var classes = this.props.classes;
-    return <div className={classes && classes.length > 0 ? classes.join(' ') : ''}>
-      <label htmlFor={this.props.selectId}>{this.props.label}</label>
-      <select id={this.props.selectId} placeholder={this.props.placeholder}></select>
-    </div>
+    return React.createElement("div", {className: classes && classes.length > 0 ? classes.join(' ') : ''}, 
+      React.createElement("label", {htmlFor: this.props.selectId}, this.props.label), 
+      React.createElement("select", {id: this.props.selectId, placeholder: this.props.placeholder})
+    )
   }
 });
